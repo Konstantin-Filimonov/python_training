1b425269a007523cb00e85deb773587e61a030d8
Konstantin <153834776+Konstantin-Filimonov@users.noreply.github.com>
2025-03-14 17:14:49 +0600

    2.3 Декомпозиция Созданы пакеты для разных зон ответственности
    Выделен класс для управления сессиями
    
    Выделен класс для работы с группами
    
    Добавлено удаление теста
    
    ---------------
    Добавление контакта
    
    Повышена читаемость кода
    
    Добавлена фикстура, убран лишний код, повышена читаемость
    
    ---------------------------------
    Созданы пакеты для разных зон ответственности
    
    Выделен класс для управления сессиями
    
    Добавлен метод удаления контакта, реализован запуск всех тестов одновременно через один браузер
    
    Добавлен метод модификации группы, внесены изменения в фикстуру, повышен интеллект метода login
    
    Исправлена ситуация с удалением группы, которая отсутствует
    
    Оптимизация теста: ускорена работа тестов
    
    Добавлен метод модификации контактов
    
    Добавлена интеллектуальная фикстура в контакты
    
    Интеллектуализированы методы login и logout в контактах
    
    Исправлена ситуация с удалением контакта, который отсутствует
    
    Реализованы проверки путём сравнения списков в тестах для создания, удаления и модификации групп и контактов.
    
    Добавлено сравение списков групп
    
    Сравнение списка групп до изменения со списком полсе изменения
    
    Добавлено кэширование и хеширование в группы с целью оптимизировать выполнение тестов
    
    Добавлено рандомное удаление и рандомная модификация групп по индексу

--- Group/fixture/group.py
+++ Group/fixture/group.py
@@ -34,22 +34,32 @@ class GroupHelper:
             driver.find_element(By.NAME, field_name).send_keys(text)
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         driver = self.app.driver
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         #submit deletion
         driver.find_element(By.NAME, "delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
+
     def select_first_group(self):
         driver = self.app.driver
         driver.find_element(By.NAME, "selected[]").click()
 
-    def modify_first_group(self, new_group_data):
+    def select_group_by_index(self, index):
+        driver = self.app.driver
+        driver.find_elements(By.NAME, "selected[]")[index].click()
+
+    def modify_group_by_index(self, index, new_group_data):
         driver = self.app.driver
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         driver.find_element(By.NAME, "edit").click()
         # fill group form

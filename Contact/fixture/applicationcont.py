d1c404e536ee45d251b0aac4c463bdb4cb32aad6
Konstantin <153834776+Konstantin-Filimonov@users.noreply.github.com>
2025-03-25 15:11:12 +0600

    2.3 Декомпозиция Созданы пакеты для разных зон ответственности
    Выделен класс для управления сессиями
    
    Выделен класс для работы с группами
    
    Добавлено удаление теста
    
    ---------------
    Добавление контакта
    
    Повышена читаемость кода
    
    Добавлена фикстура, убран лишний код, повышена читаемость
    
    ---------------------------------
    Созданы пакеты для разных зон ответственности
    
    Выделен класс для управления сессиями
    
    Добавлен метод удаления контакта, реализован запуск всех тестов одновременно через один браузер
    
    Добавлен метод модификации группы, внесены изменения в фикстуру, повышен интеллект метода login
    
    Исправлена ситуация с удалением группы, которая отсутствует
    
    Оптимизация теста: ускорена работа тестов
    
    Добавлен метод модификации контактов
    
    Добавлена интеллектуальная фикстура в контакты
    
    Интеллектуализированы методы login и logout в контактах
    
    Исправлена ситуация с удалением контакта, который отсутствует
    
    Реализованы проверки путём сравнения списков в тестах для создания, удаления и модификации групп и контактов.
    
    Добавлено сравение списков групп
    
    Сравнение списка групп до изменения со списком полсе изменения
    
    Добавлено кэширование и хеширование в группы с целью оптимизировать выполнение тестов
    
    Добавлено рандомное удаление и рандомная модификация групп по индексу
    
    Добавлено сравнение списка контактов до добавления/изменения и после
    
    Рандомное удаление и модификация (с подвохом)
    
    Оптимизирована работа со строками в группах и контактах
    
    Добавлен новый тест test_phones в контактах
    
    Переработан тест test_phones, добавлена склейка
    
    Создан генератор случайных тестовых данных в группах
    
    Реализована выборка браузера для запуска тестов групп
    
    Создан генератор случайных тестовых данных в контактах
    
    Реализована выборка браузера для запуска тестов в контактах

--- Contact/fixture/applicationcont.py
+++ Contact/fixture/applicationcont.py
@@ -7,15 +7,22 @@ from Group.fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
-        service = Service(executable_path=r'C:\chromedriver\chromedriver.exe')
-        self.driver = webdriver.Chrome(service=service)
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "edge":
+            self.driver = webdriver.Edge()
+        else:
+            raise ValueError("unrecognized browser %s" % browser)
+
         self.driver.implicitly_wait(5)
-        self.base_url = "https://www.google.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
         self.session = SessionHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -28,12 +35,11 @@ class Application:
         service = Service(executable_path=r'C:\chromedriver\chromedriver.exe')
         self.driver = webdriver.Chrome(service=service)
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
 
     def open_home_page(self):
-        self.driver.get("http://localhost/addressbook/")
+        self.driver.get(self.base_url)
 
     def open_php_my_admin(self):
         self.driver.get("http://localhost/phpmyadmin/index.php?route=/sql&db=addressbook&table=addressbook&pos=0")
